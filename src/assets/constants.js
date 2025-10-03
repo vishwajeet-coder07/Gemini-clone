e284c104d1b38a8e92a750285cb7cfc82d26e0f6
Vishwajeet-coder07 <vishwajeetrajput282@gmail.com>
2025-10-04 00:52:24 +0530

    setup api env file

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,4 +1,1 @@
-// Environment variables are automatically loaded by Vite
-const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
-
-export const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+export const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${import.meta.env.VITE_GEMINI_API_KEY}`;
